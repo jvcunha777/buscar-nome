05d5ff41dac8c35e6c945f367be67028f9d70604
JOAO VITOR DA SILVA CUNHA <112199293+jvcunha777@users.noreply.github.com>
2022-09-05 19:09:14 -0300

    Delete buscador_de_nome.py

--- buscador_de_nome.py
+++
@@ -1,8 +0,0 @@
-minha_lista = ['joao','vitor','cunha']
-minha_tupla = ('joao','vitor','cunha')
-meu_conjunto = {'joao','vitor'}
-
-if input('escreva:') in minha_tupla:
-    print('joao esta')
-else:
-    print('nao esta')
