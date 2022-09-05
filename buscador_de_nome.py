c30aec202740046f6308391af6d01f954daa7b9e
JOAO VITOR DA SILVA CUNHA <112199293+jvcunha777@users.noreply.github.com>
2022-09-05 13:41:33 -0300

    Add files via upload

---
+++ buscador_de_nome.py
@@ -0,0 +1,8 @@
+minha_lista = ['joao','vitor','cunha']
+minha_tupla = ('joao','vitor','cunha')
+meu_conjunto = {'joao','vitor'}
+
+if input('escreva:') in minha_tupla:
+    print('joao esta')
+else:
+    print('nao esta')
